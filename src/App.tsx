cd68ba2eb666aa2bda89682fc9c83d2307530b40
Joel Azemar <joel.azemar@gmail.com>
2020-11-03 10:22:24 +0100

    Fix relative path

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ const App: React.FC = () => (
           <Route path="/tab2" component={Tab2} exact={true} />
           <Route path="/tab3" component={Tab3} />
           <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+          <Redirect from="*" to="tab/1" />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
