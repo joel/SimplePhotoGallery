9a1a171e806aedb8b9bda8d346c30c1f922e7953
Joel Azemar <joel.azemar@gmail.com>
2020-11-03 13:20:05 +0100

    Remove relative path

--- src/App.tsx
+++ src/App.tsx
@@ -39,22 +39,21 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-          <Redirect from="*" to="tab/1" />
+          <Route path='/tab1' component={Tab1} exact={true} />
+          <Route path='/tab2' component={Tab2} exact={true} />
+          <Route path='/tab3' component={Tab3} />
+          <Route path='/' render={() => <Redirect to='/tab1' />} exact={true} />
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
+        <IonTabBar slot='bottom'>
+          <IonTabButton tab='tab1' href='/tab1'>
             <IonIcon icon={triangle} />
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
+          <IonTabButton tab='tab2' href='/tab2'>
             <IonIcon icon={ellipse} />
             <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
+          <IonTabButton tab='tab3' href='/tab3'>
             <IonIcon icon={square} />
             <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
